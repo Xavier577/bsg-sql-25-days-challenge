543ec1aee7c220cda25e22c27fa0f1a12d6c6337
joseph <josephtsegen10@gmail.com>
2023-02-08 04:27:47 +0100

    use FETCH FIRST ROW ONLY instead of FETCH FIRST 1 ROWS ONLY

--- day2/index.sql
+++ day2/index.sql
@@ -1,9 +1,30 @@
-WITH
-  best_goal_scorer
-  AS
-  (
-    SELECT
-      CONCAT('{"id":', id,
+WITH best_goal_scorer AS (
+   SELECT 
+    CONCAT('{"id":', id,
+    ', "nationality":"', nationality, 
+    '", "fifa_ranking":', fifa_ranking, 
+    ', "national_team_kit_sponsor":"', 
+    national_team_kit_sponsor, '", "position":"', 
+    position, '", "national_team_jersey_number":', 
+    national_team_jersey_number, ', "player_dob":"', 
+    player_dob, '", "club":"', club, '", "player_name":"',
+    player_name, '", "appearances":', 
+    appearances, ', "goals_scored":', 
+    goals_scored, ', "assists_provided":', 
+    assists_provided, ', "dribbles_per_90":', 
+    dribbles_per_90, ', "interceptions_per_90":', 
+    interceptions_per_90, ', "tackles_per_90":', 
+    tackles_per_90, ', "total_duels_won_per_90":', 
+    total_duels_won_per_90, ', "save_percentage":', 
+    save_percentage, ', "clean_sheets":', 
+    clean_sheets, ', "brand_sponsorbrand_used":"', 
+    brand_sponsorbrand_used, '"}') AS player  FROM wc_players
+  WHERE goals_scored NOT IN ('N.A', 'N,A')
+  ORDER BY goals_scored DESC
+  FETCH FIRST ROW ONLY
+), best_assist_provider AS (
+  SELECT 
+    CONCAT('{"id":', id,
     ', "nationality":"', nationality, 
     '", "fifa_ranking":', fifa_ranking, 
     ', "national_team_kit_sponsor":"', 
@@ -22,43 +43,14 @@ WITH
     save_percentage, ', "clean_sheets":', 
     clean_sheets, ', "brand_sponsorbrand_used":"', 
     brand_sponsorbrand_used, '"}') AS player
-    FROM wc_players
-    WHERE goals_scored NOT IN ('N.A', 'N,A')
-    ORDER BY goals_scored DESC
-  
-
-FETCH FIRST 1 ROWS ONLY
-), best_assist_provider AS
-(
-  SELECT
-  CONCAT('{"id":', id,
-    ', "nationality":"', nationality, 
-    '", "fifa_ranking":', fifa_ranking, 
-    ', "national_team_kit_sponsor":"', 
-    national_team_kit_sponsor, '", "position":"', 
-    position, '", "national_team_jersey_number":', 
-    national_team_jersey_number, ', "player_dob":"', 
-    player_dob, '", "club":"', club, '", "player_name":"',
-    player_name, '", "appearances":', 
-    appearances, ', "goals_scored":', 
-    goals_scored, ', "assists_provided":', 
-    assists_provided, ', "dribbles_per_90":', 
-    dribbles_per_90, ', "interceptions_per_90":', 
-    interceptions_per_90, ', "tackles_per_90":', 
-    tackles_per_90, ', "total_duels_won_per_90":', 
-    total_duels_won_per_90, ', "save_percentage":', 
-    save_percentage, ', "clean_sheets":', 
-    clean_sheets, ', "brand_sponsorbrand_used":"', 
-    brand_sponsorbrand_used, '"}') AS player
-FROM wc_players
-WHERE goals_scored NOT IN ('N.A', 'N,A')
-ORDER BY assists_provided DESC
-FETCH FIRST 1 ROWS ONLY
+  FROM wc_players
+  WHERE goals_scored NOT IN ('N.A', 'N,A')
+  ORDER BY assists_provided DESC
+  FETCH FIRST ROW ONLY
 ),
-best_player AS
-(
-   SELECT
-  CONCAT('{"id":', id,
+best_player AS (
+   SELECT 
+    CONCAT('{"id":', id,
     ', "nationality":"', nationality, 
     '", "fifa_ranking":', fifa_ranking, 
     ', "national_team_kit_sponsor":"', 
@@ -77,18 +69,15 @@ best_player AS
     save_percentage, ', "clean_sheets":', 
     clean_sheets, ', "brand_sponsorbrand_used":"', 
     brand_sponsorbrand_used, '"}') AS player
-FROM wc_players
-WHERE goals_scored NOT IN ('N.A', 'N,A')
+  FROM wc_players
+  WHERE goals_scored NOT IN ('N.A', 'N,A')
   AND assists_provided NOT IN ('N.A', 'N,A')
-ORDER BY goals_scored DESC, 
+  ORDER BY goals_scored DESC, 
   assists_provided DESC
-FETCH FIRST 1 ROWS ONLY
+  FETCH FIRST ROW ONLY
 )
-  SELECT 'overall_best_player' AS title, best_player.*
-  FROM best_player
+SELECT 'overall_best_player' AS title, best_player.* FROM best_player
 UNION
-  SELECT 'highest_goal_scorer' AS title, best_goal_scorer.*
-  FROM best_goal_scorer
+SELECT 'highest_goal_scorer' AS title, best_goal_scorer.* FROM best_goal_scorer
 UNION
-  SELECT 'highest_assist_provider' AS title, best_assist_provider.*
-  FROM best_assist_provider;
+SELECT 'highest_assist_provider' AS title, best_assist_provider.* FROM best_assist_provider;
